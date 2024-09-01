ca4cf3feba5af94d09f875f0ccd1702d9b656796
elijah-afriyie <elijah.afry@gmail.com>
2024-09-01 21:59:47 +0000

    Add Job entity

--- src/main/java/com/xlciie/jobapp/job/Job.java
+++ src/main/java/com/xlciie/jobapp/job/Job.java
@@ -1,4 +1,117 @@
 package com.xlciie.jobapp.job;
 
+import jakarta.persistence.*;
+
+import static jakarta.persistence.GenerationType.SEQUENCE;
+
+@Entity(name = "Job")
+@Table(name = "job_table")
 public class Job {
+	@Id
+	@SequenceGenerator(
+			name = "job_sequence",
+			sequenceName = "job_sequence",
+			allocationSize = 1
+	)
+	@GeneratedValue(
+			strategy = SEQUENCE,
+			generator = "job_sequence"
+	)
+	@Column(name = "id", nullable = false, columnDefinition = "BIGINT")
+	private Long id;
+
+	@Column(name = "title", nullable = false, columnDefinition = "TEXT")
+	private String title;
+
+	@Column(name = "description", nullable = false, columnDefinition = "TEXT")
+	private String description;
+
+	@Column(name = "min_salary", nullable = false, columnDefinition = "TEXT")
+	private String minSalary;
+
+	@Column(name = "max_salary", nullable = false, columnDefinition = "TEXT")
+	private String maxSalary;
+
+	@Column(name = "location", nullable = false, columnDefinition = "TEXT")
+	private String location;
+
+	public Job() {
+	}
+
+	public Job(String title, String description, String minSalary, String maxSalary, String location) {
+		this.title = title;
+		this.description = description;
+		this.minSalary = minSalary;
+		this.maxSalary = maxSalary;
+		this.location = location;
+	}
+
+	public Job(Long id, String title, String description, String minSalary, String maxSalary, String location) {
+		this.id = id;
+		this.title = title;
+		this.description = description;
+		this.minSalary = minSalary;
+		this.maxSalary = maxSalary;
+		this.location = location;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public String getMinSalary() {
+		return minSalary;
+	}
+
+	public void setMinSalary(String minSalary) {
+		this.minSalary = minSalary;
+	}
+
+	public String getMaxSalary() {
+		return maxSalary;
+	}
+
+	public void setMaxSalary(String maxSalary) {
+		this.maxSalary = maxSalary;
+	}
+
+	public String getLocation() {
+		return location;
+	}
+
+	public void setLocation(String location) {
+		this.location = location;
+	}
+
+	@Override
+	public String toString() {
+		return "Job{" +
+				"id=" + id +
+				", title='" + title + '\'' +
+				", description='" + description + '\'' +
+				", minSalary='" + minSalary + '\'' +
+				", maxSalary='" + maxSalary + '\'' +
+				", location='" + location + '\'' +
+				'}';
+	}
 }
--- src/main/java/com/xlciie/jobapp/job/impl/JobServiceImpl.java
+++ src/main/java/com/xlciie/jobapp/job/impl/JobServiceImpl.java
@@ -1,4 +1,66 @@
 package com.xlciie.jobapp.job.impl;
 
-public class JobServiceImpl {
+import com.xlciie.jobapp.job.Job;
+import com.xlciie.jobapp.job.JobRepository;
+import com.xlciie.jobapp.job.JobService;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class JobServiceImpl implements JobService {
+	private final JobRepository jobRepository;
+
+	public JobServiceImpl(JobRepository jobRepository) {
+		this.jobRepository = jobRepository;
+	}
+
+	@Override
+	public List<Job> findAllJobs() {
+		return jobRepository.findAll();
+	}
+
+	@Override
+	public Optional<Job> findJobById(Long id) {
+		Optional<Job> job = jobRepository.findById(id);
+		if (job.isPresent()) {
+			return job;
+		} else {
+			throw new RuntimeException("Job with id " + id + " not found");
+		}
+	}
+
+	@Override
+	public void addJob(Job job) {
+		jobRepository.save(job);
+	}
+
+	@Override
+	public void updateJob(Long id, Job updatedJob) {
+		Optional<Job> existingJob = jobRepository.findById(id);
+		if (existingJob.isPresent()) {
+			Job job = existingJob.get();
+
+			job.setTitle(updatedJob.getTitle());
+			job.setDescription(updatedJob.getDescription());
+			job.setMinSalary(updatedJob.getMinSalary());
+			job.setMaxSalary(updatedJob.getMaxSalary());
+			job.setLocation(updatedJob.getLocation());
+
+			jobRepository.save(job);
+		} else {
+			throw new RuntimeException("Job with id " + id + " not found");
+		}
+	}
+
+	@Override
+	public void deleteJob(Long id) {
+		Optional<Job> job = jobRepository.findById(id);
+		if (job.isPresent()) {
+			jobRepository.delete(job.get());
+		} else {
+			throw new RuntimeException("Job with id " + id + " not found");
+		}
+	}
 }
